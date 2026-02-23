24e4e314aee4ca4f38b6b6b460497d68990c9a16
dhmme <hmoony123@gmail.com>
2026-02-23 12:35:09 +0300

    update for home page

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class CustomerApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // هنا واجهة العملاء (مو العمالة)
-      home:  WorkerBookingsPage(),
+      home:  CustomerHomePage(),
     );
   }
 }
