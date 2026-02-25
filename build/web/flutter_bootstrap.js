9d36249d43498d89af8e029ddb684ffaf39efaef
dhmme <hmoony123@gmail.com>
2026-02-25 17:19:01 +0300

    222

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1964211932"
+    serviceWorkerVersion: "3395563581"
   }
 });
